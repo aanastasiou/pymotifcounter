d683909bda6c9494772582b2564bf6c3d81707f5
Athanasios Anastasiou <athanastasiou@gmail.com>
2021-11-02 10:20:22 +0000

    Added the result transformer, need to form the abstraction for the input to the process too

--- doc/source/index.rst
+++ doc/source/index.rst
@@ -12,17 +12,22 @@ The motivation to create this project was twofold:
 
 1. Over the years, I frequently found myself having to use one or more of these programs 
    from within Python over large graph data sets and had to deal with all their special 
-   points separately.
+   points separately. This includes the round-trip of having to:
+      1. Convert a given network to the format that is suitable for a given motif count algorithm
+      2. Raise the process and feed it parameters and the network at its input
+      3. Collect the output
+      4. Parse the output in a computable form (a step usually referred to as "Marshalling").
    
 2. Some of these programs represent really excellent work from the point of view of 
    establishing and analysing the counting algorithms. Unfortunately, as the time 
-   goes by and code remains unmaintained it also becomes difficult to use effectively.
+   goes by and code remains unmaintained it also becomes difficult to use effectively, 
+   for someone who sets off from zero knowledge.
    
 The broad aim of this piece of work is to offer Python *bindings* to the underlying 
 data structures and code for each one of these algorithms.
 
 The objective at this stage of development is to provide an interface at process level 
-that abstracts away each algorithm's input, output and parameters. 
+that abstracts away each algorithm's input, output and parameters.
 
 All that the user interacts with is a top level Python object that returns the motif 
 distribution in a way that makes it easy to use these results further within a Python 
--- var_src/main.py
+++ var_src/main.py
@@ -44,7 +44,8 @@ import os
 import re
 import networkx
 import subprocess
-import typing
+import pyparsing
+import pandas
 
 class PyMotifCounterException(Exception):
     pass
@@ -98,34 +99,18 @@ class Parameter:
         # return f"-{self._name} {str(self._value)}"
         return ["-"+self._name, str(self._value)]
         
-class MotifCountResultBase:
-    def __init__(self, ctx):
-        raise Exception("Not implemented")
-        # # Process theoutput (if succesfull)
-        # # TODO:HIGH, need to inspect the `err` and raise appropriate errors
-        # outputData = self._output_parser.parseString(out)
-        # D = pandas.DataFrame(columns=list(outputData["zscore"][0].keys()) + ["ave_rand_freq_sd", "ave_rand_conc_sd"], index = None)
-        # for an_item in enumerate(outputData["zscore"]):
-            # D.at[an_item[0], "gID"] = an_item[1]["gID"]
-            # D.at[an_item[0], "freq"] = an_item[1]["freq"]
-            # D.at[an_item[0], "conc"] = an_item[1]["conc"]
-            # D.at[an_item[0], "f-ZScore"] = an_item[1]["f-ZScore"]
-            # D.at[an_item[0], "f-pValue"] = an_item[1]["f-pValue"]
-            # D.at[an_item[0], "c-ZScore"] = an_item[1]["c-ZScore"]
-            # D.at[an_item[0], "c-pValue"] = an_item[1]["c-pValue"]
-            # D.at[an_item[0], "ave_rand_freq"] = an_item[1]["ave_rand_freq"]["ave_rand_freq"]
-            # D.at[an_item[0], "ave_rand_conc"] = an_item[1]["ave_rand_conc"]["ave_rand_conc"]
-            # D.at[an_item[0], "ave_rand_freq_sd"] = an_item[1]["ave_rand_freq"]["sd"]
-            # D.at[an_item[0], "ave_rand_conc_sd"] = an_item[1]["ave_rand_conc"]["sd"]
-            
-        # with open("adjMat.txt", "rt") as fd:
-            # adj_mat_data = fd.read()
-        # parsed_adjacency_results = self._adjacency_matrix_parser.parseString(adj_mat_data)
-        # identified_motifs = {}
-        # for an_item in parsed_adjacency_results:
-            # identified_motifs[an_item["graphID"]] = an_item["G"]
 
+class PyMotifCounterResultBase:
+    def __init__(self):
+        pass
         
+    def __call__(self, a_ctx, a_graph):
+        """
+        Actually performs the conversion and returns a dataframe of results.
+        """
+        return None
+
+       
 class PyMotifCounterProcessBase:
     """
     Represents an external process.
@@ -138,6 +123,7 @@ class PyMotifCounterProcessBase:
             
         self._parameters = parameters
         self._binary_location = binary_location
+        self._result_transformer = PyMotifCounterResultBase() 
         
     def add_parameter(self, a_param):
         if a_param._name in self._parameters or a_param._alias in self._parameters:
@@ -212,12 +198,69 @@ class PyMotifCounterProcessBase:
         ctx = self._before_run(a_graph)
         ctx = self._run(ctx, a_graph)
         ctx = self._after_run(ctx, a_graph) #ctx must also contain the entire file returned by the algorithm
-        return ctx
-        # return MotifCountResultBase(ctx)  
+        return self._result_transformer(ctx, a_graph)
+
+        
+class PyMotifCounterResultNetMODE(PyMotifCounterResultBase):
+    @staticmethod
+    def _get_parser():
+        """
+        Returns a parser that is used to transform the output of a given algorithm 
+        to computable form.
+        
+        Notes:
+            * This is usually a pyparsing parser.
+        """
+        float_num = pyparsing.Regex(r"([+-]?)(nan|([0-9]*)\.([0-9]+))").setParseAction(lambda s, l ,t:float(t[0]))
+        int_num = pyparsing.Regex(r"[0-9]+").setParseAction(lambda s,l,t:int(t[0]))
+        gID = (pyparsing.Suppress("gID:") + int_num("gID"))
+        freq = (pyparsing.Suppress("freq:") + int_num("freq"))
+        ave_rand_freq = pyparsing.Group(pyparsing.Suppress("ave_rand_freq:") + float_num("ave_rand_freq") + pyparsing.Suppress("(sd:") + float_num("sd") + pyparsing.Suppress(")"))("ave_rand_freq")
+        conc = (pyparsing.Suppress("conc:") + float_num("conc"))
+        ave_rand_conc = pyparsing.Group(pyparsing.Suppress("ave_rand_conc:") + float_num("ave_rand_conc") + pyparsing.Suppress("(sd:") + float_num("sd") + pyparsing.Suppress(")"))("ave_rand_conc")
+        fzscore = (pyparsing.Suppress("f-ZScore:") + float_num("f-ZScore"))
+        fpvalue = (pyparsing.Suppress("f-pValue:") + float_num("f-pValue"))
+        czscore = (pyparsing.Suppress("c-ZScore:") + float_num("c-ZScore"))
+        cpvalue = (pyparsing.Suppress("c-pValue:") + float_num("c-pValue"))
+        row_data = pyparsing.Group(gID + freq + ave_rand_freq + conc + ave_rand_conc + fzscore + fpvalue + czscore + cpvalue)
+        data_parsing = (pyparsing.Suppress(r"calc Z-Score") + pyparsing.ZeroOrMore(row_data))("zscore")
+        return data_parsing
+            
+    def __init__(self):
+        self._parser = self._get_parser()
+        
+    def __call__(self, a_ctx, a_graph):
+        # Process theoutput (if succesfull)
+        # TODO:HIGH, need to inspect the `err` and raise appropriate errors
+        outputData = self._parser.parseString(a_ctx["proc_response"])
+        ret_dataframe = pandas.DataFrame(columns=list(outputData["zscore"][0].keys()) + ["ave_rand_freq_sd", "ave_rand_conc_sd"], index = None)
+        for an_item_idx, an_item in enumerate(outputData["zscore"]):
+            ret_dataframe.at[an_item_idx, "gID"] = an_item["gID"]
+            ret_dataframe.at[an_item_idx, "freq"] = an_item["freq"]
+            ret_dataframe.at[an_item_idx, "conc"] = an_item["conc"]
+            ret_dataframe.at[an_item_idx, "f-ZScore"] = an_item["f-ZScore"]
+            ret_dataframe.at[an_item_idx, "f-pValue"] = an_item["f-pValue"]
+            ret_dataframe.at[an_item_idx, "c-ZScore"] = an_item["c-ZScore"]
+            ret_dataframe.at[an_item_idx, "c-pValue"] = an_item["c-pValue"]
+            ret_dataframe.at[an_item_idx, "ave_rand_freq"] = an_item["ave_rand_freq"]["ave_rand_freq"]
+            ret_dataframe.at[an_item_idx, "ave_rand_conc"] = an_item["ave_rand_conc"]["ave_rand_conc"]
+            ret_dataframe.at[an_item_idx, "ave_rand_freq_sd"] = an_item["ave_rand_freq"]["sd"]
+            ret_dataframe.at[an_item_idx, "ave_rand_conc_sd"] = an_item["ave_rand_conc"]["sd"]
+        return ret_dataframe    
+        # with open("adjMat.txt", "rt") as fd:
+            # adj_mat_data = fd.read()
+        # parsed_adjacency_results = self._adjacency_matrix_parser.parseString(adj_mat_data)
+        # identified_motifs = {}
+        # for an_item in parsed_adjacency_results:
+            # identified_motifs[an_item["graphID"]] = an_item["G"]
         
 class PyMotifCounterNetMODE(PyMotifCounterProcessBase):
     def __init__(self):
+        # Build the base model
         super().__init__(binary_location="../binaries/NetMODE/NetMODE")
+        # Exchange the result transformer
+        self._result_transformer = PyMotifCounterResultNetMODE()
+        # Add the right parameters        
         self.add_parameter(Parameter(name="k", \
                                      alias="motif_size", \
                                      help_str="k-node subgraphs (=3,4,5 or 6)", \
@@ -245,8 +288,6 @@ class PyMotifCounterNetMODE(PyMotifCounterProcessBase):
         for a_param_value in all_param_values:
             p_params.extend(a_param_value())
             
-        import pdb
-        pdb.set_trace()
         # Create the process object
         # p = subprocess.Popen([f"{self._binary_location}NetMODE", "-k", f"{self._knodesize}", "-e", f"{self._edge_random_method}", "-c", f"{self._nrandom}"], universal_newlines=True, stdin = subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         p = subprocess.Popen([self._binary_location] + p_params, universal_newlines=True, stdin = subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -257,14 +298,21 @@ class PyMotifCounterNetMODE(PyMotifCounterProcessBase):
         ret_ctx.update(ctx)
         ret_ctx.update({"proc_response":out, \
                         "proc_error":err})
-        return ret_ctx        
-
-# class PyMotifNetMODECounter(PyMotifCounterProcessBase):
-    # """
-    # Represents a NetMODE call.
-    # """
-    # def __init__(self):
-        # self.parameters.append(Parameter(name="k", help_str="Motif size", validation_exr=re.compile("3|4|5
+        return ret_ctx
+        
+    def _after_run(self, ctx, a_graph):
+        """
+        Performs any cleanup after the process has run and produced results.
+        
+        Notes:
+            * NetMODE creates an adjacency matrix file in the CWD with all 
+              the adjacency matrices of the motifs it enumerated. This is 
+              not strictly needed because the adjacency matrix can be inferred 
+              by the motif's ID.
+        """
+        if os.path.exists("adjMat.txt"):
+            os.remove("adjMat.txt")
+        return ctx
 
 if __name__ == "__main__":
     # v1 = Parameter(name="s", long_name="specify", help_str="Specify length", validation_expr=re.compile("[0-9]+"), is_required=False, param_value=23)
